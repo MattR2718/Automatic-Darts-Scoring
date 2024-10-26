160efd80edc11d736452b70037f66c4d96e87f40
JamBur05 <james3906@gmail.com>
2024-10-26 15:50:30 +0100

    Remove Pickle

--- Client/Client/Client.cpp
+++ Client/Client/Client.cpp
@@ -19,7 +19,7 @@ int main()
         boost::asio::io_context io_context;
 
         //std::string server_ip = boost::asio::ip::host_name();
-        std::string server_ip = "192.168.138.121";  // Change address !!
+        std::string server_ip = "192.168.138.80";  // Change address !!
 
         ClientClass client(io_context, server_ip);
 
