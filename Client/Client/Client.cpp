3f6792c5bba6b1ce0d49f369536974f3fa4daeb4
JamBur05 <james3906@gmail.com>
2024-10-26 13:01:56 +0100

    C++ Client Implemented

--- Client/Client/Client.cpp
+++ Client/Client/Client.cpp
@@ -3,13 +3,40 @@
 
 #include "Client.h"
 #include <fmt/core.h>
+#include <boost/asio.hpp>
+#include <iostream>
+#include <string>
+
+const int HEADER = 64; // Bytes for the length of the message
+const int PORT = 5050;
+const std::string DISCONNECT_MESSAGE = "!DISCONNECT";
+const std::string FORMAT = "utf-8";
+
+#include "ClientClass.h"
+
+using boost::asio::ip::tcp;
 
 using namespace std;
 
 int main()
 {
-	fmt::print("HELLO FMT\n");
+    try {
+        boost::asio::io_context io_context;
 
-	cout << "Hello CMake." << endl;
-	return 0;
+        // Set server IP
+        std::string server_ip = boost::asio::ip::host_name();  // Gets localhost
+
+        ClientClass client(io_context, server_ip);
+
+        // Send messages to server
+        client.send("Hello World!");
+        client.send("HI YIPPIE!");
+        client.send(DISCONNECT_MESSAGE);
+
+    }
+    catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
 }
