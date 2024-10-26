a4f2ae71456e33fde2ac771e43fbce0470287d81
Himanshu Dias <himanshudias2004@gmail.com>
2024-10-26 12:38:53 +0100

    added threshold

--- Server/image_processing.py
+++ Server/image_processing.py
@@ -13,9 +13,16 @@ while True:
         break
 
     fgMask = sBackSub.apply(frame)
+    fgMask_th = cv.threshold(fgMask, 200,255, cv.THRESH_BINARY)[1]
 
+    #original stream
     cv.imshow('Stream',frame)
+
+    #stream with background substracted
     cv.imshow('Mask',fgMask)
+
+    #background substracted with lighter colours set to white
+    cv.imshow("Threshold mask",fgMask_th)
 
 
     #quit stream
