f50a682fdb82bdcbc9101f9d02c94b52d708516a
MattR2718 <matthew.ranson2718@gmail.com>
2024-10-27 02:15:47 +0000

    switch to float

--- Client/Client/mt_client.h
+++ Client/Client/mt_client.h
@@ -4,6 +4,7 @@
 #include <atomic>
 #include <string>
 #include <mutex>
+#include <iostream>
 
 class DoubleClient {
 public:
@@ -55,32 +56,32 @@ private:
 
 
 
-// main.cpp
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <server_ip> <port>\n";
-        std::cerr << "Example: " << argv[0] << " 192.168.1.100 12345\n";
-        return 1;
-    }
-
-    DoubleClient client(argv[1], argv[2]);
-    client.start();
-
-    // Example: periodically check for new values
-    double value1, value2;
-    try {
-        while (true) {
-            if (client.getLatestValues(value1, value2)) {
-                std::cout << "Main thread got values: "
-                    << value1 << ", " << value2 << std::endl;
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        }
-    }
-    catch (std::exception& e) {
-        std::cerr << "Main thread error: " << e.what() << std::endl;
-        return 1;
-    }
-
-    return 0;
-}
+//// main.cpp
+//int main(int argc, char* argv[]) {
+//    if (argc != 3) {
+//        std::cerr << "Usage: " << argv[0] << " <server_ip> <port>\n";
+//        std::cerr << "Example: " << argv[0] << " 192.168.1.100 12345\n";
+//        return 1;
+//    }
+//
+//    DoubleClient client(argv[1], argv[2]);
+//    client.start();
+//
+//    // Example: periodically check for new values
+//    double value1, value2;
+//    try {
+//        while (true) {
+//            if (client.getLatestValues(value1, value2)) {
+//                std::cout << "Main thread got values: "
+//                    << value1 << ", " << value2 << std::endl;
+//            }
+//            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+//        }
+//    }
+//    catch (std::exception& e) {
+//        std::cerr << "Main thread error: " << e.what() << std::endl;
+//        return 1;
+//    }
+//
+//    return 0;
+//}
--- Server/mt_server.py
+++ Server/mt_server.py
@@ -39,7 +39,7 @@ class Server:
                         try:
                             # Check for new data to send with a timeout
                             doubles = self.send_queue.get(timeout=0.1)
-                            data = struct.pack('dd', doubles[0], doubles[1])
+                            data = struct.pack('ff', doubles[0], doubles[1])
                             connection.sendall(data)
                             print(f"Sent values: {doubles[0]}, {doubles[1]}")
                         except queue.Empty:
